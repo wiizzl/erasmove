0913f6d7b4ceb123d0507f98245c108bd130d728
wiizzl <git@houlliere.com>
2026-03-04 14:03:42 +0100

    (#2) Initialisation du projet

---
+++ Erasmove/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Erasmove;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Erasmove/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Erasmove;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
